963932953c61472aa1d36d8beed63f45856d559c
Pavithra Suresh <pavithrasuresh399@gmail.com>
2020-11-29 03:50:58 +0530

    Blog post

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     });
 
+
+    /* owl carousel for blog */
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots:false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+
+    
 });
