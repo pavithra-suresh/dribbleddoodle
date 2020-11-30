40465536afe84c4b7a2d6e5cfb7d5f324dcf6661
Pavithra Suresh <pavithrasuresh399@gmail.com>
2020-12-01 05:23:14 +0530

    autoplay seconds

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop: true,
         autoplay: true,
-        autoplayTimeout: 3000,
+        autoplayTimeout: 5000,
         dots: false,
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
