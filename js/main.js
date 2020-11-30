8b34d5ec52c149179f212d4d45fc2722d168aa8a
Pavithra Suresh <pavithrasuresh399@gmail.com>
2020-11-30 19:12:27 +0530

    footer

--- js/main.js
+++ js/main.js
@@ -19,5 +19,15 @@ $(document).ready(function(){
         navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
     });
 
+
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 3000);
+    })
+
+
     
 });
